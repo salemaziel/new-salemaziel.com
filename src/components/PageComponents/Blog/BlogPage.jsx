4dec5b2bf99a94dfa874749fe0095475601cc588
salemaziel <mymainemail0501@gmail.com>
2020-10-05 16:36:13 -0700

    up

---
+++ src/components/PageComponents/Blog/BlogPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import HomeHeader from './HomeHeader';
+import SectionTwo from './sectionTwo';
+import SectionThree from './sectionThree'
+
+const BlogPage = () => (
+  <>
+    <div id="main">
+
+          <SectionTwo />
+
+
+    </div>
+
+  </>
+);
+
+export default BlogPage;
